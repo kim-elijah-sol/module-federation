f01f3f50bee61caa42b8d24f35d67c6701245fe1
kim-elijah-sol <kimelijahsol@gmail.com>
2023-12-31 22:22:23 +0900

    fix: [cra-a] index.html response 제거

--- cra-a/server.js
+++ cra-a/server.js
@@ -4,7 +4,7 @@ const path = require('node:path')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, 'dist'))
 })
 
 app.use(express.static(path.join(__dirname, 'dist')))
