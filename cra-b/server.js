44f7305265d58cf0ffacd5e12dbed57ae205a929
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-01 23:05:36 +0900

    feat: [cra-b] 기본 설정

---
+++ cra-b/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('node:path')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist'))
+})
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+process.on('SIGINT', () => {
+  process.exit(0)
+})
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3002 [cra-b]')
+})
---
+++ cra-b/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>CRA B App</h1>
+}
+
+export default App
