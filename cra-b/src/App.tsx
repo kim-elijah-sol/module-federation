b058c44375e0997ef05145359a119d79412fe0aa
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-01 23:15:41 +0900

    feat: [cra-b] craA App 렌더링 추가

--- cra-b/src/App.tsx
+++ cra-b/src/App.tsx
@@ -1,9 +1,17 @@
-import { lazy } from 'react'
+import { Suspense, lazy } from 'react'
 
+// @ts-ignore
 const CraAApp = lazy(() => import('craA/App'))
 
 function App() {
-  return <h1>CRA B App</h1>
+  return (
+    <div>
+      <h1>CRA B App</h1>
+      <Suspense fallback={<div>loading craA App</div>}>
+        <CraAApp />
+      </Suspense>
+    </div>
+  )
 }
 
 export default App
